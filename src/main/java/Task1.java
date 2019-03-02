cb50006e03f797dca3bf225bb0faadedc7034c6d
Marek Sandomierski <m.sandomierski@gmail.com>
2019-03-02 08:37:13 +0100

    task1

---
+++ src/main/java/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+
+
+    public static void main(String[] args) {
+
+        sayHello("Marek");
+
+    }
+
+    private static void sayHello(String name) {
+
+        System.out.println("Cześć " + name);
+
+    }
+}
